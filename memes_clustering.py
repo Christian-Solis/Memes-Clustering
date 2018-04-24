4d030c4846bdca394bb6739d1ec9f35f39488d5f
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-04-24 14:13:57 -0500

    Start of the Parser and datasets

---
+++ memes_clustering.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------
+# Name: Christian Ricardo Solís Cortés
+# Student ID: A01063685
+# Artificial Intelligence
+# Assignment: Final Project
+# Memes recommendation by clustering techniques
+# -----------------------------------------------------------------------------
