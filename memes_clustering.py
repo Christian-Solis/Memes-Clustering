b34bd340fc694d15554790aac238c12b5f15e22c
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-04-24 17:47:49 -0500

    Reading the file correctly

--- memes_clustering.py
+++ memes_clustering.py
@@ -5,3 +5,49 @@
 # Assignment: Final Project
 # Memes recommendation by clustering techniques
 # -----------------------------------------------------------------------------
+
+import heapq
+import itertools
+import sys
+import os
+
+# -----------------------------------------------------------------------------
+# Read the dataset file if it changes
+# -----------------------------------------------------------------------------
+
+def read_file(filename):
+    file_to_read = open(filename)
+    print(file_to_read.read())
+    file_to_read.close()
+
+# -----------------------------------------------------------------------------
+# Parsing and cleaning process
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# Main function
+# -----------------------------------------------------------------------------
+
+def main():
+    # data_set_1 = open('datasets/memes_dataset.in','r')
+    # data_set_2 = open('memes_dataset_2.in','r')
+
+    # Specify the file directory to separate datasets from main program
+    file_dir = os.path.dirname(os.path.realpath('__file__'))
+    # print('File dir ↓', '\n')
+    # print(file_dir)
+
+    # Accessing the file in the folder contained in the current folder
+    data_set_1 = os.path.join(file_dir, 'datasets/memes_dataset.in')
+    read_file(data_set_1)
+
+    # Check if file can be read
+    # with open('memes_dataset.in') as f:
+    #     first_line = f.readline()
+    #     print(first_line)
+
+    # input_reading(data_set_1)
+
+if __name__ == '__main__':
+    main()
