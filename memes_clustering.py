aa92a467d9aee48e5b9f09dbfebf41e832d3879e
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-04-26 19:43:37 -0500

    Parsing of second dataset

--- memes_clustering.py
+++ memes_clustering.py
@@ -11,21 +11,26 @@ import itertools
 import sys
 import os
 import collections
-# import nltk
+import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
 # -----------------------------------------------------------------------------
-# Read the dataset file if it changes
+# Open the first dataset file
 # -----------------------------------------------------------------------------
 
-def read_file(filename):
+def read_file_1(filename):
     file_to_read = open(filename)
-    # print(file_to_read.read())
-    # file_to_read.close()
     input_reading_1(file_to_read)
-    # input_reading_2(file_to_read)
+
+# -----------------------------------------------------------------------------
+# Open the second dataset
+# -----------------------------------------------------------------------------
+
+def read_file_2(filename):
+    file_to_read = open(filename)
+    input_reading_2(file_to_read)
 
 # -----------------------------------------------------------------------------
 # Parsing and cleaning process of first dataset
@@ -68,14 +73,50 @@ def input_reading_1(dataset):
         text_of_implementation.append(text)
         # date_of_meme.append(date)
 
-    print(date_of_meme)
-
-    # #return(stack_of_nodes, number_of_probabilities, probabilities, number_of_queries, queries)
+    # print(text_of_implementation)
+    dataset.close()
+    # return(stack_of_nodes, number_of_probabilities, probabilities, number_of_queries, queries)
 
 # -----------------------------------------------------------------------------
-# Parsing and cleaning process of second dataset
+# Parsing and cleaning process of first dataset
 # -----------------------------------------------------------------------------
 
+# Clean the dataset to get only the necessary info
+def input_reading_2(dataset):
+    # Temporal copy of dataset
+    temp_dataset = dataset
+    # Read the dataset
+    dataset_read = dataset.read()
+    # Split input by spaces
+    splitted_input_by_line_break = dataset_read.split('\n')
+    # list of variables
+    id_of_meme = list()
+    url_slug_of_meme = list()
+    meme_name = list()
+    url_of_meme = list()
+
+    # For loop that extracts the content for each variable
+    for line in splitted_input_by_line_break[:-1]:
+        # Split input by tabs
+        splitted_input_by_tabs = line.split('\t')
+        # ID of meme
+        id_meme = splitted_input_by_tabs[0]
+        # URL slug of meme
+        slug = splitted_input_by_tabs[1]
+        # Meme name
+        name = splitted_input_by_tabs[2]
+        # URL of meme
+        url = splitted_input_by_tabs[3]
+
+        # Append content to corresponding lists
+        id_of_meme.append(id_meme)
+        url_slug_of_meme.append(slug)
+        meme_name.append(name)
+        url_of_meme.append(url)
+
+    print(url_of_meme)
+    dataset.close()
+    # return(stack_of_nodes, number_of_probabilities, probabilities, number_of_queries, queries)
 
 # -----------------------------------------------------------------------------
 # Word tokenizer that converts text into tokens
@@ -101,14 +142,11 @@ def word_tokenizer(text_of_meme):
 def main():
     # Specify the file directory to separate datasets from main program
     file_dir = os.path.dirname(os.path.realpath('__file__'))
-    # print('File dir ↓', '\n')
-    # print(file_dir)
-
     # Accessing the file in the folder contained in the current folder
     data_set_1 = os.path.join(file_dir, 'datasets/memes_dataset.in')
-    # data_set_2 = os.path.join(file_dir, 'datasets/memes_dataset_2.in')
-    read_file(data_set_1)
-    # read_file(data_set_2)
+    data_set_2 = os.path.join(file_dir, 'datasets/memes_dataset_2.in')
+    read_file_1(data_set_1)
+    read_file_2(data_set_2)
 
 if __name__ == '__main__':
     main()
